41fba236ff985a8b33d47683f5ae00d3aa04f152
Antti <karvanen.antti@gmail.com>
2019-12-06 19:31:16 +0200

    day1

--- day01/day1.py
+++ day01/day1.py
@@ -1,12 +1,11 @@
-import math
 with open('input.txt', 'r') as fp:
     line = fp.readline()
     sum = 0
     while line:
-        fuel = math.floor(int(line.strip())/3) - 2
+        fuel = int(line.strip()) // 3 - 2
         while (fuel > 0):
             sum += fuel
-            fuel = math.floor(fuel/3) - 2
+            fuel = fuel // 3 - 2
         line = fp.readline()
         
 
